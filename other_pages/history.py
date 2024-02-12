b6b944f4426918728e76a2c1118680459643c758
yeomko22 <hjkim2246@gmail.com>
2024-02-12 21:22:39 +0900

    Complete history page.

--- other_pages/history.py
+++ other_pages/history.py
@@ -2,37 +2,31 @@ import streamlit as st
 from services.service_db import select_questions, select_total_questions
 
 st.title("History")
-st.write("유저가 마켓 옥토퍼스에게 물어본 질문들과 그에 대한 답변을 조회합니다.")
+st.write("유저가 마켓 옥토퍼스에게 물어본 질문들을 조회합니다. 클릭하면 답변을 확인할 수 있습니다.")
 
 total_questions = select_total_questions()
 total_pages = total_questions // 10 + 1
 
+st.markdown("""
+<style>
+[data-testid="stLinkButton"] a {
+    display: flex;
+    justify-content: flex-start;
+}
+</style>
+""", unsafe_allow_html=True)
 cols = st.columns(4)
 with cols[0]:
     selected_page = st.selectbox(
         label="페이지",
-        options=[x+1 for x in range(10)],
+        options=[x+1 for x in range(total_pages)],
         key="page",
     )
 
 total_questions = select_questions(page=selected_page)
-cols = st.columns([0.1, 0.1, 0.7, 0.1])
-with cols[0]:
-    st.markdown("id")
-with cols[1]:
-    st.markdown("created_at")
-with cols[2]:
-    st.markdown("question")
-with cols[3]:
-    st.markdown("detail")
-
 for id_, question, created_at in total_questions:
-    with cols[0]:
-        st.write(id_)
-    with cols[1]:
-        st.write(created_at)
-    with cols[2]:
-        st.write(question)
-
-
-
+    st.link_button(
+        label=f"{id_}\. ({created_at.strftime('%Y-%m-%d')}) {question}",
+        url=f"/question_answer/{id_}",
+        use_container_width=True,
+    )
